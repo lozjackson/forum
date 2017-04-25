32640211127dcf45b8cc473fd2393ef1f0195cb4
Loz Jackson <lozjackson@talktalk.net>
2017-04-25 23:50:54 +0100

    update routes

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('topic');
+  this.route('topic', { path: '/topic/:topic_id' });
   this.route('new-topic');
 });
 
--- app/routes/topic.js
+++ app/routes/topic.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('topic');
+const { Route } = Ember;
+
+export default Route.extend({
+  breadCrumb: {},
+  afterModel(model) {
+    this._super(...arguments);
+    this.set('breadCrumb', { title: model.get('title') });
   }
 });
