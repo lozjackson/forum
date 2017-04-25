af6ee71fab57d3e8967a177b87d819f489651e57
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 00:28:49 +0100

    add topic controller

---
+++ app/controllers/topic.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  edit() {
+    this.transitionToRoute('topic.edit');
+  }
+});
---
+++ tests/unit/controllers/topic-test.js
@@ -0,0 +1,10 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:topic', 'Unit | Controller | topic', {
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
