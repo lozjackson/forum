5e70013fe03302df4bb1e6c4eac291fcdc8a76ad
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 23:46:11 +0100

    update svg-icon component

--- app/components/svg-icon.js
+++ app/components/svg-icon.js
@@ -7,7 +7,7 @@ export default Component.extend({
   tagName: 'svg',
   attributeBindings: ['height', 'width', 'viewBox', 'fill'],
   viewBox: '0 0 24 24',
-  size: 24,
+  size: 30,
 
   height: alias('size'),
   width: alias('size'),
