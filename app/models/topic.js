51d3f730bd2e11aec70c142168d25047753e011c
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 23:50:07 +0100

    update topic model

--- app/models/topic.js
+++ app/models/topic.js
@@ -1,6 +1,10 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
+
+const { computed } = Ember;
+const { mapBy } = computed;
 
 export default Model.extend({
   created: 	attr('string', { defaultValue: function () {
@@ -13,4 +17,14 @@ export default Model.extend({
   body: attr('string'),
   author: belongsTo('user', { async: true }),
   posts: hasMany('post', { async: true }),
+
+  postDates: mapBy('posts', 'created'),
+
+  lastPost: computed('postDates', 'created', function () {
+    const postDates = this.get('postDates');
+    if (!postDates.get('length')) {
+      return this.get('created');
+    }
+    return postDates.sort().get('lastObject');
+  })
 });
