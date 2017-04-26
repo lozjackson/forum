12897995e1368b6105f6115bd10efdd3e6052038
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 13:23:42 +0100

    update topic index controller

--- app/controllers/topic/index.js
+++ app/controllers/topic/index.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const { computed, inject: { service }, Controller, RSVP: { Promise } } = Ember;
+
+export default Controller.extend({
+
+  sessionAccount: service(),
+
+  canEditTopic: computed('model.author', 'sessionAccount.user', function () {
+    return new Promise(resolve => {
+      this.get('model.author').then(author => resolve(author === this.get('sessionAccount.user')));
+    });
+  }),
+
   edit() {
     this.transitionToRoute('topic.edit');
   }
