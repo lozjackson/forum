a2978eeafd00e1db0396f2e7636a3a1a0ba8a45e
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 13:24:09 +0100

    update new-topic route

--- app/routes/new-topic.js
+++ app/routes/new-topic.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+const { inject: { service }, Route } = Ember;
+
+export default Route.extend(AuthenticatedRouteMixin, {
+
+  sessionAccount: service(),
+
   model() {
-    return this.store.createRecord('topic');
+    return this.store.createRecord('topic', {
+      author: this.get('sessionAccount.user')
+    });
   }
 });
