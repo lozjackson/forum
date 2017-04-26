5bf72f0905cb6bf9c286fb0bca98baa1ac5e2f0a
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 23:50:21 +0100

    update index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+
+  queryParams:{
+		search:{
+			refreshModel: true
+		}
+	},
+
+  model(params) {
+    if (params && params.search) {
+      return this.store.query('topic', params);
+    }
     return this.store.findAll('topic');
   }
 });
