01a9cb6f98e87d2b92026bd10976ec61f6ce400d
Loz Jackson <lozjackson@talktalk.net>
2017-04-26 10:44:37 +0100

    update application adapter

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -1,10 +1,15 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('adapter:application', 'Unit | Adapter | application', {
-  // needs: ['serializer:foo']
+  needs: ['service:session']
 });
 
 test('it exists', function(assert) {
   let adapter = this.subject();
   assert.ok(adapter);
+});
+
+test('has correct authorizer', function(assert) {
+  let adapter = this.subject();
+  assert.equal(adapter.authorizer, 'authorizer:oauth2');
 });
