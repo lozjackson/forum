a52f57a96d28f1eb4fd84880cc5626c2d1287327
Loz Jackson <lozjackson@talktalk.net>
2017-05-07 14:29:27 +0100

    update user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,14 @@
-import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 
-const { computed: { readOnly } } = Ember;
-
 export default Model.extend({
   username: attr('string'),
+  name: attr('string'),
   email: attr('string'),
+  disabled: attr('boolean'),
 
   topics: hasMany('topic', { async: true }),
   posts: hasMany('post', { async: true }),
-
-  name: readOnly('username')
+  roles: hasMany('role', { async: true })
 });
