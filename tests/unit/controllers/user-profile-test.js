6353618d7be70e6aa2b7754e7783e7a71020ac42
Loz Jackson <lozjackson@talktalk.net>
2017-05-07 15:17:34 +0100

    add user-profile controller

---
+++ tests/unit/controllers/user-profile-test.js
@@ -0,0 +1,10 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:user-profile', 'Unit | Controller | user profile', {
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
