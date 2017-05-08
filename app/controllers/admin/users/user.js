abee142bb25ebd84a222da8bdcf77cda96d899c7
Loz Jackson <lozjackson@talktalk.net>
2017-05-08 10:06:14 +0100

    update admin/users/user

--- app/controllers/admin/users/user.js
+++ app/controllers/admin/users/user.js
@@ -1,39 +1,4 @@
 import Ember from 'ember';
-import UserProfileController from 'forum/controllers/user-profile';
-import getErrorResponse from 'forum/utils/get-error-response';
 
-const { computed, computed: { alias }, inject: { service } } = Ember;
-
-export default UserProfileController.extend({
-
-  ajax: service(),
-  dialog: service(),
-  sessionAccount: service(),
-
-  roles: alias('changeset.roles'),
-
-  admin: computed('roles.@each.name', {
-    get() {
-      return this.get('roles').findBy('name', 'admin');
-    },
-    set(key, value) {
-      this.manageRole(value?'add':'remove', 'admin');
-      return value;
-    }
-  }),
-
-  addRole(name) {
-    this.manageRole('add', name);
-  },
-
-  removeRole(name) {
-    this.manageRole('remove', name);
-  },
-
-  manageRole(action, name) {
-    const url = `/users/${ this.get('model.id') }/${ action }-role`;
-    this.get('ajax').put({ url, data: { name } }).then((data) => {
-      this.store.pushPayload(data);
-    }, (error) => this.get('dialog').alert({ title: 'Error', body: getErrorResponse(error) }));
-  }
+export default Ember.Controller.extend({
 });
--- app/routes/admin/users/user.js
+++ app/routes/admin/users/user.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+const { computed, Route } = Ember;
+
+export default Route.extend(AuthenticatedRouteMixin, {
+  breadCrumb: computed('controller.model.username', function () {
+    return { title: this.controller.get('model.username') || 'Untitled' }
+  })
+});
